006269d8805da4ac01654d92f5307cbebd14ae91
kfiala <Kent.Fiala@gmail.com>
2024-03-20 15:52:32 -0400

    First pass at track downloads

--- mychecklists.js
+++ mychecklists.js
@@ -2,6 +2,10 @@ if (window.location.href.includes('/mychecklists')) {
 	if (sessionStorage.getItem('doCountEm') || sessionStorage.getItem('showAllHref')) {
 		if (checkURL())
 			countem();
+	} else if (sessionStorage.getItem('doMapEm')) { 
+		console.log('Found doMapEm set');
+		mapem();
+		sessionStorage.removeItem('doMapEm');
 	} else {
 		addChecklistsButton();
 	}
@@ -48,12 +52,14 @@ function addChecklistsButton() {	// Add our main menu button
 	dropDiv.append(myH4);
 
 	let itemObject1 = menuItem('Check for flagged checklists');
-	let itemObject2 = menuItem('Help');
-	let itemObject3 = menuItem('Cancel');
+	let itemObject2 = menuItem('Download all GPS tracks');
+	let itemObjectH = menuItem('Help');
+	let itemObjectC = menuItem('Cancel');
 
 	dropDiv.append(itemObject1.item);
 	dropDiv.append(itemObject2.item);
-	dropDiv.append(itemObject3.item);
+	dropDiv.append(itemObjectH.item);
+	dropDiv.append(itemObjectC.item);
 
 	itemObject1.p.addEventListener('click', () => {
 		sessionStorage.setItem('doCountEm', 1);
@@ -61,10 +67,15 @@ function addChecklistsButton() {	// Add our main menu button
 	});
 
 	itemObject2.p.addEventListener('click', () => {
+		sessionStorage.setItem('doMapEm', 1);
+		location.reload();
+	});
+
+	itemObjectH.p.addEventListener('click', () => {
 		location.href = 'https://www.faintlake.com/eBird/extension/Enhancements/#E1';
 	});
 
-	itemObject3.p.addEventListener('click', () => {
+	itemObjectC.p.addEventListener('click', () => {
 		myDiv.remove();
 		addChecklistsButton();
 	});
@@ -164,7 +175,7 @@ function countOne(listItems, i) {
 	if (li.getAttribute('id')) {
 		checklistID = li.getAttribute('id');
 		href = li.querySelector('a').getAttribute('href').trim();
-		li.querySelector('a').setAttribute('target', '_blank');
+//?		li.querySelector('a').setAttribute('target', '_blank');
 		fetchHTML(href, checklistID);
 	}
 	if (i+1 < listItems.length) {
@@ -214,7 +225,8 @@ function displayToggle() {
 
 async function fetchHTML(path, id) {
 	let url = 'https://ebird.org' + path;
-	await fetch(url, {mode: "cors"})
+	console.log('fetchHTML', url);
+	fetch(url, {redirect: "follow"})
 		.then((response) => {
 			return response.text();
 		})
@@ -228,8 +240,134 @@ async function fetchHTML(path, id) {
 
 		})
 		.catch(function (error) {
-			console.log('Error on ' + id);
+			console.log('Error on ', id);
 			console.log(error);
 		});
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let trackList = {};
+let subIdList = [];
 
+function mapem() {
+	let listItems = document.getElementById('place-species-observed-results').querySelectorAll('li.ResultsStats');
+	if (!document.getElementById('myDivId'))
+		addChecklistsButton();
+
+	mapOne(listItems, 0);
+	displayToggle();
+}
+
+function mapOne(listItems, i) {
+	let li = listItems[i];
+	if (li.getAttribute('id')) {
+//?		li.querySelector('a').setAttribute('target', '_blank');
+		fetchGPS(li.querySelector('a').getAttribute('href').trim(), li.getAttribute('id'));
+	}
+	if (i + 1 < listItems.length) {
+		setTimeout(() => {
+			mapOne(listItems, i + 1);
+		}, 235);
+	} else {	// At the end
+		downloadTracks(subIdList);
+	}
+}
+
+async function fetchGPS(path, id) {
+	let url = 'https://ebird.org' + path;
+	console.log('Fetching', url);
+	fetch(url)
+	.then((response) => {
+		return response.text();
+	})
+	.then((data) => {
+		let subId = url.slice(url.lastIndexOf('/') + 1);
+		let checklistTitleA = data.match('<title>.*</title>');	// Get the html title
+		let checklistTitle = checklistTitleA[0].slice(7, -8);
+
+		// Find the checklist URL
+		let URLoffset = data.search('<link rel="canonical" href=".*">');
+		if (URLoffset) {
+			let URL = data[URLoffset + '<link rel="canonical" href="'.length];
+			URL = URL.split('"')[0];
+		}
+
+		if (data.search('<h3 id="flagged"') < 0) {	// Exclude flagged checklists
+			let offset = data.indexOf('data-maptrack-data');	// Look for the GPS data in the text
+			if (offset > 0) {	// If it's there, process it
+				let linend = data.indexOf('"', offset + 'data-maptrack-data="'.length + 2);
+				data = data.slice(offset + 'data-maptrack-data="'.length, linend);
+				storeTrack(subId, { points: data, title: checklistTitle, URL: URL });
+				document.getElementById(id).style.backgroundColor = '#8f8';
+			} else {
+				document.getElementById(id).style.backgroundColor = 'yellow';
+				document.getElementById(id).classList.add('noTrack');
+			}
+		}
+	})
+	.catch(function (error) {
+		console.log('Error on ' + id);
+		console.log('URL', url);
+		console.log(error);
+	});
+}
+
+function storeTrack(subId, checklistObject) {	// Take the string of coordinates and turn them into xml
+	let ar = checklistObject.points.split(',');	// Convert the coordinate string into an array
+	let trackPoint = [];	// Set up the array that we will put in the xml
+	for (let i = 0, c = 0; i < ar.length; i += 2, c++) {
+		trackPoint[c] = '<trkpt lon="' + ar[i] + '" lat="' + ar[i + 1] + '"></trkpt>';
+	}
+//	sessionStorage.setItem(subId, '<trk><name>' + subId + '</name><desc><![CDATA[' + checklistObject.URL + ' ' + checklistObject.title + ']]></desc><trkseg>' + trackPoint.reduce(joiner) + '</trkseg></trk>');
+	trackList[subId] = '<trk><name>' + subId + '</name><desc><![CDATA[' + checklistObject.URL + ' ' + checklistObject.title + ']]></desc><trkseg>' + trackPoint.reduce(joiner) + '</trkseg></trk>';
+	subIdList.push(subId);
+
+	function joiner(complete, element) {
+		return complete + element;
+	}
+}
+
+async function downloadTracks(subIdList) { // Wait for all the xml to be ready then save it
+	let total = 0;
+	let XML = '<?xml version="1.0" encoding="UTF-8" ?><gpx version="1.1" creator="eBird"><metadata>'
+		+ '<name><![CDATA[' + document.title + ']]></name>'
+		+ '<desc><![CDATA[' + document.title + ']]></desc>'
+		+ '</metadata>';
+
+
+	for (let i = 0; i < subIdList.length; i++) {
+		let subId = subIdList[i];
+		let track = trackList[subId];
+		if (track) {
+			XML += track;
+			total += track.length;
+		}
+	}
+	XML += '</gpx>';
+
+	if (!total) {	// If no tracks found
+		setTimeout(abort, 50);
+		return;
+	}
+
+	console.log('Finishing');
+	// Set up a dummy anchor for downloading the xml file, then click it
+	const link = document.createElement('a')
+	link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(XML))
+	link.setAttribute('download', document.title + '.gpx');
+	//	link.style.display = 'none';
+
+	link.append('Click here');
+
+	let thing = document.getElementById('myChecklistsForm');
+	thing.append(link);
+	console.log('Appended link to thing');
+	//	document.body.appendChild(link);
+	link.click();
+	//	document.body.removeChild(link);
+
+	function abort() {
+		console.log('No tracks');
+	}
+}
