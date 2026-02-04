f9a3b7b8cddcce142be204cfa2149ac6ed73570e
kfiala <Kent.Fiala@gmail.com>
2026-02-04 17:39:14 -0500

    Support for iconic species

--- regionPage.js
+++ regionPage.js
@@ -2,13 +2,15 @@ if (window.location.pathname.includes('region/') || window.location.href.include
 	// Don't redirect if the first two path elements are the same as in referrer
 	// In Firefox but not Chrome, document.referrer is empty string on a redirect; check for length of 0
 
-	let pathArray = document.referrer.split('/').slice(3);	// slice off protocol and domain
+	let referrer = document.referrer.split('?')[0]; // Strip off any query parameters
+	
+	let pathArray = referrer.split('/').slice(3);	// slice off protocol and domain
 	let referringPath = pathArray[0] + '/' + pathArray[1];
 
 	pathArray = window.location.pathname.split('/').slice(1);	// slice off leading '/'
 	let currentPath = pathArray[0] + '/' + pathArray[1];
 
-	if (document.referrer.length && currentPath !== referringPath) {
+	if (referrer.length && currentPath !== referringPath) {
 		reDirect();	// We are coming from a different page, so it is first entry, redirect if needed
 	} else {
 		finishRedirect(); // This should be the second entry, after the redirect. No further redirects.
@@ -24,32 +26,32 @@ function finishRedirect() {
 }
 
 function reDirect() {
-	if (!options.regionView) {
-		console.log('Waiting for options to load');
-		setTimeout(reDirect, 100);
-	}
-	if (!document.getElementById('ebirding-this-month')) {
-		console.log('Waiting for page to load');
+	if (!Object.hasOwn(options, 'regionView')) {
 		setTimeout(reDirect, 100);
 	} else {
-		if (options.regionView != 'Month') {	// Redirect unless they are asking for the default
-			switchTo(options.regionView);
-		} else {
-			addRegionButton();	// Default so no redirect, just add our button.
-		}
+		let target = setTarget();
+		switchTo(target);
 	}
+
 }
 
 function switchTo(target) {	// Redirect to the desired view
 	let URLarray = window.location.pathname.slice(1).split('/');
 	let url = window.location.origin + '/' + URLarray[0] + '/' + URLarray[1];	// Keep only the first two elements of the path
-	console.log('origin', window.location.origin, 'URLarray', URLarray, 'url', url);
 
 	switch (target) {
 		case 'Month':
 			break;
 		case 'Year':
 			url += '?yr=cur';
+			break;
+		case 'IconicMonth':
+			if (window.location.href.includes('hotspot/'))
+				url += '/iconic-birds?yr=curM'; 
+			break;
+		case 'IconicYear':
+			if (window.location.href.includes('hotspot/'))
+				url += '/iconic-birds';
 			break;
 		case 'Bird List':
 			url += '/bird-list';
@@ -59,7 +61,6 @@ function switchTo(target) {	// Redirect to the desired view
 			break;
 		default:
 	}
-	console.log('Switching to', window.location.href = url);
 	window.location.href = url;
 }
 
@@ -69,9 +70,26 @@ function selectListener() {
 	options.regionView = selected;
 	saveOptions();
 
-	switchTo(selected);
+	let target = setTarget();
+	switchTo(target);
 }
-	
+
+function setTarget() {
+	let target = options.regionView;
+	if (window.location.href.includes('region/')) { // Regions don't do iconic
+		switch (target) {
+			case "IconicMonth":
+				target = 'Month';
+				break;
+			case "IconicYear":
+				target = 'Year';
+				break;
+			default:
+		}
+	}
+	return target;
+}
+
 function addRegionButton() {
 	const focusColor = '#eeddbb';
 
@@ -124,41 +142,26 @@ function addRegionButton() {
 		select.addEventListener('change', selectListener);
 		select.append(optionNull);
 
-		let option1 = document.createElement('option');
-		option1.setAttribute('value', 'Month');
-		if (options.regionView == 'Month') {
-			option1.style.backgroundColor = focusColor;
-		}
-		option1.append('This Month');
-		select.append(option1);
+		function setupOption(code, display) {
 
-		let option2 = document.createElement('option');
-		option2.setAttribute('value', 'Year');
-		if (options.regionView == 'Year') {
-			option2.style.backgroundColor = focusColor;
+			let option = document.createElement('option');
+			option.setAttribute('value', code); // 'IconicMonth'
+			if (options.regionView == code) {
+				option.style.backgroundColor = focusColor;
+			}
+			option.append(display);	// 'Iconic Birds--This Month'
+			select.append(option);
 		}
-		option2.append('This Year');
-		select.append(option2);
 
-		let option3 = document.createElement('option');
-		option3.setAttribute('value', 'Bird List');
-		if (options.regionView == 'Bird List') {
-			option3.style.backgroundColor = focusColor;
-		}
-		option3.append('Bird List');
-		select.append(option3);
-
-		let option4 = document.createElement('option');
-		option4.setAttribute('value', 'Recent Checklists');
-		if (options.regionView == 'Recent Checklists') {
-			option4.style.backgroundColor = focusColor;
-		}
-		option4.append('Recent Checklists');
-		select.append(option4);
+		setupOption('Month', 'Overview--This Month');
+		setupOption('Year', 'Overview--This Year');
+		setupOption('IconicMonth', 'Iconic Birds--This Month');
+		setupOption('IconicYear', 'Iconic Birds--All Months');
+		setupOption('Bird List', 'Bird List');
+		setupOption('Recent Checklists', 'Recent Checklists');
 
 		containerDiv.appendChild(myDiv);
 	} else {
-		console.log('Waiting to add region button');
 		setTimeout(500, addRegionButton);
 	}
 }
