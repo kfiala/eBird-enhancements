1dc763dab6520803921349791a2aa78c3ec28bc5
kfiala <Kent.Fiala@gmail.com>
2026-07-04 10:31:18 -0400

    Backup/restore checklistMediaDialog

--- commonFunctions.js
+++ commonFunctions.js
@@ -1,12 +1,6 @@
-if (window.location.pathname.includes('/checklist/') && window.location.href.endsWith('/media')) {
-	const isFirefox = typeof InstallTrigger !== 'undefined';
-	if (isFirefox) { // Because Firefox tends to lose track of localStorage
-		localStorage.setItem('checklistMediaDialog', 'true');
-	}
-}
-
 var options = {};
 getOptions();
+setChecklistMediaDialog();
 
 async function getOptions() {
 	try {
@@ -41,6 +35,23 @@ async function getOptions() {
 		}
 	} catch (error) {
 		console.error('Error retrieving value:', error);
+	}
+}
+
+async function setChecklistMediaDialog() {
+	// Firefox sometimes loses track of localStorage if you clear history, so keep a backup.
+	const checklistMediaDialog = localStorage.getItem('checklistMediaDialog');
+	// if checklistMediaDialog is saved as expected, just back it up.
+	if (checklistMediaDialog) {
+		chrome.storage.local.set({ 'checklistMediaDialog': checklistMediaDialog });
+	} else {
+		// checklistMediaDialog was not present as expected, so fetch backup value.
+		const response = await (chrome.storage.local.get(['checklistMediaDialog']));
+		backupChecklistMediaDialog = response['checklistMediaDialog'];
+		if (typeof backupChecklistMediaDialog !== 'undefined') {
+			// checklistMediaDialog was backed up, so restore it to localStorage.
+			localStorage.setItem('checklistMediaDialog', backupChecklistMediaDialog);
+		} 
 	}
 }
 
